59c22f0f889db38a01b479a18fe22f326928e925
Ivan Codasquieves <ivan@>
2022-02-11 13:25:11 -0300

    Add log on response

--- src/interceptors/http-response-interceptor.ts
+++ src/interceptors/http-response-interceptor.ts
@@ -1,7 +1,9 @@
+import { Logger } from "@codasquieves/logger";
 import type { Response } from "express";
 import { injectable } from "inversify";
 import type { Action, InterceptorInterface } from "routing-controllers";
 import { Interceptor } from "routing-controllers";
+import type { RequestContainer } from "../types";
 import { HttpResponse } from "../entities/http-response";
 
 @Interceptor()
@@ -13,6 +15,16 @@ class HttpResponseInterceptor implements InterceptorInterface {
     if (!(result instanceof HttpResponse)) {
       return result;
     }
+
+    const request = action.request as RequestContainer;
+
+    const logger = request.ioc.get(Logger);
+
+    logger.debug("Response", {
+      body: result.body,
+      headers: result.headers,
+      status: result.statusCode,
+    });
 
     result.headers.forEach(([key, value]) => response.set(key, value));
     response.status(result.statusCode);
